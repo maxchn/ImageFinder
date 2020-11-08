4737db794c95eccc5a9f4e942d30afccfa53e493
Maxim Chesnokov <32434213+maxchn@users.noreply.github.com>
2020-11-08 17:46:01 +0200

    Add placeholder

--- app/src/main/java/com/imagefinder/features/featuresMain/common/BindingAdapter.kt
+++ app/src/main/java/com/imagefinder/features/featuresMain/common/BindingAdapter.kt
@@ -4,11 +4,13 @@ import androidx.databinding.BindingAdapter
 import androidx.appcompat.widget.AppCompatImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.imagefinder.R
 
 @BindingAdapter("app:imageUrl")
 fun loadImage(view: AppCompatImageView, url: String) {
     Glide.with(view.context)
         .load(url)
         .diskCacheStrategy(DiskCacheStrategy.ALL)
+        .placeholder(R.drawable.ic_placeholder)
         .into(view)
 }
